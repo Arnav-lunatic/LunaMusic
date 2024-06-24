99d160af3fd84ee72659f9755f4226267ff0231b
Arnav-lunatic <arnav4c@gmail.com>
2024-06-24 20:59:07 +0530

    fix minute bugs

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,18 @@ import { Outlet } from "react-router-dom";
 import { SearchContext } from "./context/SearchContext";
 import { Tooltip } from "react-tooltip";
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
+import default_thumbnail_50x50 from '/src/assets/default_thumbnail_50x50.jpg'
+import default_thumbnail_500x500 from '/src/assets/default_thumbnail_500x500.jpg'
 
 function App() {
 	const [searchData, setSearchData] = useState([]);
 	const [currentTrack, setCurrentTrack] = useState({
-		path: "./src/assets/default.wav",
+		path: "/src/assets/default.wav",
 		id: 1,
 		name: "No track found",
-		thumbnail_50x50: "./src/assets/default_thumbnail_50x50.jpg",
-		thumbnail_500x500: "./src/assets/default_thumbnail_500x500.jpg",
+		thumbnail_50x50: default_thumbnail_50x50,
+		thumbnail_500x500: default_thumbnail_500x500,
 		artist: "No artist found",
 		year: 2024,
 		duration: 0,
